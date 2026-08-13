2feb169303ad4f480455b0e113f6a27f135bb254
kbirket <kbirket@pattersonhc.org>
2026-08-13 13:27:39 -0500

    Update page.tsx

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { createClient } from "@/lib/supabase/client";
+import { createClient } from "../../lib/supabase/client";
 import "./order.css";
 
 type Drink = {
